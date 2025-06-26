021e8f508f82c9aedbd098e3afa5d722c94d2574
yuzusim <yuzusim@gmail.com>
2025-06-26 22:49:13 +0900

    컴포넌트 동적 렌더링 : 배열을 map으로 순회하며 각 요소를 props로 넘겨 하나의 컴포넌트를 동적으로 여러 번 렌더링할 수 있다.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
 
-function Food({ favourite }) {
-  return <h3>I like {favourite}</h3>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h3>I like {name}</h3>
+      <img src={picture} style={{ width: 200, height: "auto" }} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+  },
+  {
+    name: "Bulgogi",
+    image: "https://images.unsplash.com/photo-1525351484163-7529414344d8",
+  },
+  {
+    name: "samgiopsal",
+    image:
+      "https://godomall.speedycdn.net/0f1081f2872a5b54f665bd623a5689cb/goods/1190/image/detail/1190_detail_053.jpg",
+  },
+];
 
 function App() {
   return (
     <div>
-      <h2>Hello!!!</h2>
-      <Food favourite="kimchi" />
-      <Food favourite="ramen" />
-      <Food favourite="samgiopsal" />
-      <Food favourite="chukumi" />
+      {foodILike.map((dish) => (
+        <Food key={dish.name} name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
