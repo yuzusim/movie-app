7e676ecaa1bbcac62f866abcc65085065a8c4255
yuzusim <yuzusim@gmail.com>
2025-06-27 03:44:33 +0900

    클래스형 컴포넌트에서 componentDidMount()를 사용해 6초 후 isLoding 상태를 false로 변경하고, 이를 삼항 연산자로 조건부 렌더링하여 Loading 또는 We are ready를 화면에 표시한다.

--- src/App.js
+++ src/App.js
@@ -3,41 +3,17 @@ import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoding: true,
   };
 
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
-
-  // 컴포넌트가 DOM에 마운트 된 직후 호출.
   componentDidMount() {
-    console.log("component rendered");
+    setTimeout(() => {
+      this.setState({ isLoding: false });
+    }, 6000);
   }
-
-  // 업데이트 후 호출. 네트워크 요청 재실행, DOM 조작 등이 여기서 가능.
-  componentDidUpdate() {
-    console.log("I just update");
-  }
-
-  // 컴포넌트가 제거되기 전에 호출
-  // 타이머 제거, 구독 해제, 이벤트 리스너 제거 같은 정리 작업을 함.
-  componentWillUnmount() {
-    console.log("Goodbye cruel world");
-  }
-
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoding } = this.state;
+    return <div>{isLoding ? "Loading" : "We are ready"}</div>;
   }
 }
 
