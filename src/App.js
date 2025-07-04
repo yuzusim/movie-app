d751522e2adf6e7c4264e3880b1f5b021909e635
yuzusim <yuzusim@gmail.com>
2025-07-04 14:42:42 +0900

    async/await로 API 응답을 기다린 뒤, setState로 영화 데이터를 state에 저장

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import axios from "axios";
 
 class App extends React.Component {
   state = {
-    isLoding: true,
+    isLoading: true,
     movies: [],
   };
+  getMovies = async () => {
+    // componentDidMount에서 data 를 fetch 하기
+    const movies = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
+    );
+  };
 
-  // 6초 뒤에 isLoding을 false로 바꿔서 화면에 We are ready를 보이게 함
-  // componentDidMount에서 data 를 fetch 하기
   componentDidMount() {
+    // 6초 뒤에 isLoding을 false로 바꿔서 화면에 We are ready를 보이게 함
     // setTimeout(() => {
     //   this.setState({ isLoding: false });
     // }, 6000);
-    axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+
+    this.getMovies();
   }
 
   render() {
-    const { isLoding } = this.state;
-    return <div>{isLoding ? "Loading" : "We are ready"}</div>;
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
   }
 }
 
