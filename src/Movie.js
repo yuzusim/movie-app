001d830b5646dcb571184438756d35fb869bbf49
yuzusim <yuzusim@gmail.com>
2025-07-08 11:47:42 +0900

    map으로 genres 배열 순회해 <li> 목록 렌더링 추가

--- src/Movie.js
+++ src/Movie.js
@@ -3,25 +3,35 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 // 매개변수 자리에서 곧바로 props를 구조분해(=인자로 받음)
-function movie({ id, year, title, summary, poster }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div class="movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div class="movie_data">
-        <h2 class="movie_title">{title}</h2>
-        <h5 class="movie_year">{year}</h5>
-        <p class="movie_summary">{summary}</p>
+      <div className="movie_data">
+        <h2 className="movie_title">{title}</h2>
+        <h5 className="movie_year">{year}</h5>
+
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="movie_genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+
+        <p className="movie_summary">{summary}</p>
       </div>
     </div>
   );
 }
 
-movie.PpopTypes = {
+Movie.PpopTypes = {
   id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-export default movie;
+export default Movie;
