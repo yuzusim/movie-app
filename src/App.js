f887dd89a7264c3eac3cf5c97017e749260b2e23
yuzusim <yuzusim@gmail.com>
2025-07-10 19:38:34 +0900

    Navigation 컴포넌트를 생성하고, Link를 통해 라우터 경로로 이동할 수 있도록 설정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Routes>
-        <Route path="/" Component={Home} />
+        <Route path="/" exact={true} Component={Home} />
         <Route path="/about" Component={About} />
       </Routes>
     </HashRouter>
